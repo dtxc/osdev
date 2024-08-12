144b5f525da7ce7a6aea05bc32b0770de74f03ac
dtxc <69575053+dtxc@users.noreply.github.com>
2024-08-13 01:12:25 +0300

    Add colored printing
    at least it should look decent till i add the next big thing

--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,5 @@
 #define SSFN_CONSOLEBITMAP_TRUECOLOR
 #include <ssfn.h>
-
-#include <stdio.h>
 #include <multiboot.h>
 
 #include <asm/io.h>
@@ -18,9 +16,12 @@ extern uint8_t _binary_console_sfn_start;
 
 static void ssfn_puts(char *s) {
     while (*s != '\0') {
+        // when *s = '\n', ssfn_putc prints a weird unicode instead of a newline.
         if (*s == '\n') {
-            ssfn_dst.y++;
+            ssfn_dst.y += 17; // 17 looks perfect for me.
             ssfn_dst.x = 1;
+            s++;
+            continue;
         }
 
         ssfn_putc(*s);
@@ -28,15 +29,21 @@ static void ssfn_puts(char *s) {
     }
 }
 
+static void ssfn_cputs(char *s, uint32_t fg) {
+    ssfn_dst.fg = fg;
+    ssfn_puts(s);
+    ssfn_dst.fg = 0xFFFFFFFF;
+}
+
 void kernel_main() {
-    ssfn_puts("hello world");
-    
+    ssfn_puts("\n\nWelcome to ");
+    ssfn_cputs("Radiant OS", 0xFF00FFFF);
+    ssfn_puts("!\n"); // just a minor detail cuz im a lil crazy for them.
+
     return;
 }
 
 void kernel_init(struct mboot_info *mboot_ptr) {
-    clear();
-
     init_serial();
     serial_puts("Serial output successfully initialized.\n");
 
@@ -46,6 +53,7 @@ void kernel_init(struct mboot_info *mboot_ptr) {
     // outb(PIC_MASTER_DAT, 0xF9);
     // outb(PIC_SLAVE_DAT, 0xFF);
 
+    // at f = 1193 Hz, T_irq = 0.999847 ms, the closest possible to 1s.
     pit_install(1193);
 
     init_paging();
@@ -56,16 +64,23 @@ void kernel_init(struct mboot_info *mboot_ptr) {
 
     ssfn_src = &_binary_console_sfn_start;
     ssfn_dst.ptr = (uint8_t*) LFB_VADDR;
-    ssfn_dst.p = vbe_info->pitch;
-    ssfn_dst.fg = 0xFFFFFFFF;
+    ssfn_dst.p = vbe_info->pitch; // bytes per line
+    ssfn_dst.fg = 0xFFFFFFFF; // white on black
     ssfn_dst.bg = 0x00000000;
     ssfn_dst.x = 1;
     ssfn_dst.y = 0;
 
+    ssfn_putc('[');
+    ssfn_cputs("+", 0xFF00FF00); // green
+    ssfn_puts("] SSFN has been configured.\n");
+
+    // enable interrupts
     asm volatile("sti");
 
     init_acpi();
-    // ssfn_puts("ACPI initialization completed.\n");
+    ssfn_putc('[');
+    ssfn_cputs("+", 0xFF00FF00); // green
+    ssfn_puts("] ACPI initialization completed.\n");
 
     kernel_main();
 }
