75a3e7073a4266feae12d88d375b3f1d4241f54e
nick <69575053+dtxc@users.noreply.github.com>
2024-08-13 13:06:26 +0300

    Add 16 bit value support for port mapped I/O
    Added inw, outw

--- arch/i386/include/asm/io.h
+++ arch/i386/include/asm/io.h
@@ -30,6 +30,7 @@
 uint8_t inb(uint16_t port);
 uint16_t inw(uint16_t port);
 void outb(uint16_t port, uint8_t val);
+void outw(uint16_t port, uint16_t val);
 
 int init_serial();
 void serial_putc(char c);
