64abfd54bd31e53fc9da3718c0fee746f65b5b0b
nick <69575053+dtxc@users.noreply.github.com>
2024-08-18 23:24:28 +0300

    vesa.c: add scrolling, improve vesa_putc()

--- drivers/display/vesa.c
+++ drivers/display/vesa.c
@@ -1,3 +1,5 @@
+#include <ssfn.h>
+#include <string.h>
 #include <asm/io.h>
 #include <video/vbe.h>
 
@@ -20,4 +22,39 @@ void init_vbe(struct vbe_mode_info *vbe_info) {
     height = vbe_info->height;
     bpp = vbe_info->bpp;
     pitch = vbe_info->pitch;
+}
+
+void vesa_puts(char *s) {
+    while (*s != '\0') {
+        vesa_putc(*s);
+        s++;
+    }
+}
+
+static void vesa_scroll() {
+    int offset = 17 * pitch;
+    int size = SCREEN_HEIGHT * pitch;
+
+    memmove((uint8_t *) lfb, (uint8_t *) lfb + offset, size - offset);
+    memset((uint8_t *) lfb + (size - offset), 0, offset);
+}
+
+void vesa_putc(char c) {
+    if (ssfn_dst.x + 9 >= SCREEN_WIDTH) {
+        ssfn_dst.y += 17;
+        ssfn_dst.x = 1;
+    }
+
+    if (ssfn_dst.y + 17 > SCREEN_HEIGHT) {
+        vesa_scroll();
+        ssfn_dst.y -= 17;
+    }
+
+    // when *s = '\n', ssfn_putc prints a weird unicode instead of a newline.
+    if (c == '\n') {
+        ssfn_dst.y += 17; // 17 looks perfect for me. the pixels i mean, i am not drake
+        ssfn_dst.x = 1;
+    } else {
+        ssfn_putc(c);
+    }
 }
