192a1720f43c0bffb1ab5f80c14d1b43d4cf4562
nick <69575053+dtxc@users.noreply.github.com>
2024-08-18 11:38:39 +0300

    string.h: add memcpy() implementation

--- include/string.h
+++ include/string.h
@@ -2,9 +2,11 @@
 
 #include <common.h>
 
-void *memset(void *dst, uint8_t c, int n);
+void memset(void *dst, uint8_t c, int n);
+void memcpy(void *dst, void *src, int num);
 
 int strcpy(char *dst, const char *src);
+int strncpy(char *dst, const char *src, int n);
 int strcmp(char *s, const char *s1);
 int strncmp(char *s, const char *s1, int n);
 int strlen(const char *s);
